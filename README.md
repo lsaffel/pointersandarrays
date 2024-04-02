f4069c567ad7f84a8dc669032cd97c95e9370fe6
Lisa <llvvss@icloud.com>
2024-04-02 00:27:38 -0600

    pointers and arrays, input and output of numbers

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Pointers and arrays  
-A few ways to refer to elements in the array
+A few ways to refer to elements in the array  
+Input the numbers from the user  
+Then write the elements of the array out
--- src/pointersandarrays.cpp
+++ src/pointersandarrays.cpp
@@ -4,12 +4,31 @@ using namespace std;
 int main()
 {
     // an array of type int
-    int luckyNumbers[5] = {2,3,5,7,9};
+    int luckyNumbers[5];
 
+    /*
     cout << luckyNumbers << endl; // result is 0x7ff7bfeff110
     cout << &luckyNumbers[0] << endl; // result is same, 0x7ff7bfeff110
     cout << luckyNumbers[2] << endl;    // result is 5
     cout << *(luckyNumbers + 2) << endl;    // same result as line above: 5
+    */
+
+   // input 5 numbers from the user into the array
+   for(int i = 0; i <= 4; i++) {
+        cout << "Number: ";
+        cin >> luckyNumbers[i];
+   }
+
+    // write out the five numbers
+    for(int i = 0; i <= 4; i++) {
+        cout << *(luckyNumbers+i) << "  ";
+   }  
+
+   // if this is written with an index out of bounds, i <= 5
+   // then a wrong element will be output 
+   // at the end of this list that is not in the array
+   //     for(int i = 0; i <= 5; i++) {
+
 
     return 0;
 }
